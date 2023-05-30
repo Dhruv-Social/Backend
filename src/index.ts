d370e3e61f9f23ec1516a29017fc10e5ba6e362e
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-05-30 13:59:16 +1200

    feat: basic structure of express app works

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import express, { Request, Response, Application } from "express";
+require("dotenv").config();
+
+const app: Application = express();
+let port: number = parseInt(process.env.API_PORT!);
+let devMode: boolean = process.env.DEV_MODE === "true" ? true : false;
+
+app.all("*", async (req: Request, res: Response) => {
+  return res.send({
+    detail: "This endpoint does not exist.",
+    endpoint: { detail: `'${req.url}' does not exist.` },
+  });
+});
+
+app.listen(port, async () => {
+  console.log(`Listening on port ${port}`);
+});
