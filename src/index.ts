904b8096e56a90316d8b818ccd9fc78e13be0edd
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-06-06 22:33:30 +1200

    feat: getSpecificUser getting the user based off the username

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
 
-import { PrismaClient } from "@prisma/client";
+// Import Routes
+import getSpecificUserRoute from "./routes/get/getSpecificUser";
 
 dotenv.config();
 
 const app: Application = express();
 const port: number = parseInt(process.env.API_PORT!);
 const devMode: boolean = process.env.DEV_MODE === "true" ? true : false;
+
+// Use Routes
+app.use("/dhruvsocial/getSpecificUser", getSpecificUserRoute);
+
+app.all("/", async (req: Request, res: Response) => {
+  return res.send({ detail: "Welcome to the Dhruv Social API" });
+});
 
 // Fallback
 app.all("*", async (req: Request, res: Response) => {
---
+++ src/routes/get/getSpecificUser.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import { Errors } from "../../core/errors/errors";
+
+const getSpecificUserRoute: Router = express.Router();
+
+// This route is to get a user based on their username
+getSpecificUserRoute.get("/", async (req: Request, res: Response) => {
+  const { username } = req.query;
+
+  // check if username is not provided
+  if (typeof username !== "string" || username === "") {
+    return res
+      .status(Errors.didNotProvideUsername().details.errorCode)
+      .send(Errors.didNotProvideUsername());
+  }
+
+  const prisma = new PrismaClient();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  return res.send(user);
+});
+
+export default getSpecificUserRoute;
