fe53649c1137ce0e3253c9c74da24f6c4d66fbef
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-06-07 18:34:09 +1200

    feat: new inherated class for custom errors

--- src/core/errors/errors.ts
+++ src/core/errors/errors.ts
@@ -1,11 +1,59 @@
 export class Errors {
-  private constructor() {}
+  constructor() {}
 
   static didNotProvideUsername() {
     return {
       success: false,
       details: {
         reason: "You did not provide the username",
+        errorCode: 400,
+      },
+    };
+  }
+}
+
+export class PostErrors extends Errors {
+  private constructor() {
+    super();
+  }
+
+  static postUserInvalidDetails() {
+    return {
+      success: false,
+      details: {
+        reason: "An item was null or undefined, try proving all the items",
+        errorCode: 400,
+      },
+    };
+  }
+
+  static postUserUsernameFailed() {
+    return {
+      success: false,
+      details: {
+        reason:
+          "Username failed checks, username can only contain numbers and alphabet",
+        errorCode: 400,
+      },
+    };
+  }
+
+  static postUserPasswordFailed() {
+    return {
+      success: false,
+      details: {
+        reason:
+          "Password failed checks, password must be greater than 7 character and can not cntain white space",
+        errorCode: 400,
+      },
+    };
+  }
+
+  static postUserLocationFailed() {
+    return {
+      success: false,
+      details: {
+        reason: "Location can only be in New Zealand",
         errorCode: 400,
       },
     };
