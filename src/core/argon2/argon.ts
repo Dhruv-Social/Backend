6c9606907371d0d9adeb40291d2b0ed237d0da6c
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-06-07 18:43:23 +1200

    doc: adding comments to functions

--- src/core/argon2/argon.ts
+++ src/core/argon2/argon.ts
@@ -1,9 +1,20 @@
 import argon2 from "argon2";
 
+/**
+ * Function To Hash a password
+ * @param password: string,
+ * @returns Promise<string> hashed password
+ */
 const hashPassword = async (password: string): Promise<string> => {
   return argon2.hash(password);
 };
 
+/**
+ * Function to verify if a password is correct
+ * @param hashedPassword: string, hashed password
+ * @param password: string, normal password
+ * @returns Promise<boolean> if the password is correct
+ */
 const verifyPassword = async (
   hashedPassword: string,
   password: string
