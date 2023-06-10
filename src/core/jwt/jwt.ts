9ea398c8a04a1321fbe42151b601b3cef6a7414f
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-06-10 14:09:27 +1200

    feat: jwt auth for user creation

---
+++ src/core/jwt/jwt.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+/**
+ * Creates a token with jwt and 64 random bytes :)))
+ * @param user Not the hashed password
+ * @return Returns a token
+ */
+let createToken = (user: any): any => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, {
+    expiresIn: "1h",
+  });
+};
+
+/**
+ * Decrypt a JWT token so you can use the values
+ * @param token jwt token
+ * @returns a dictionary of the values in the token
+ */
+let decryptToken = (token: any): any => {
+  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!);
+};
+
+let decryptTokenRefresh = (token: any): any => {
+  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!);
+};
+
+export { createToken, decryptToken, decryptTokenRefresh };
