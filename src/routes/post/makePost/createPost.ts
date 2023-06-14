d10a709fefe5b8dbcec3e939771ae009f2f24c7b
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-06-14 22:13:51 +1200

    feat: create post endpoint done

---
+++ src/routes/post/makePost/createPost.ts
@@ -0,0 +1,93 @@
+import express, { Request, Response, Router } from "express";
+import crypto from "crypto";
+import fileUpload from "express-fileupload";
+
+import { Post } from "@prisma/client";
+import { verifyArray } from "../../../core/verifyArray/verifyArray";
+import { PostErrors } from "../../../core/errors/errors";
+import { authToken } from "../../../core/auth/auth";
+import { prisma } from "../../../core/prisma/prisma";
+
+const createPost: Router = express.Router();
+
+// Reminder to get the post UUID from the token
+createPost.post(
+  "/",
+  authToken,
+  fileUpload(),
+  async (req: Request | any, res: Response) => {
+    const { text } = req.body;
+    const { uuid } = req.user;
+
+    let media1: string, media2: string, media3: string, media4: string;
+
+    try {
+      media1 = req.files.media1.data.toString("base64");
+    } catch (err: any) {
+      media1 = "null";
+    }
+
+    try {
+      media2 = req.files.media2.data.toString("base64");
+    } catch (err: any) {
+      media2 = "null";
+    }
+
+    try {
+      media3 = req.files.media3.data.toString("base64");
+    } catch (err: any) {
+      media3 = "null";
+    }
+
+    try {
+      media4 = req.files.media4.data.toString("base64");
+    } catch (err: any) {
+      media4 = "null";
+    }
+
+    const media: string[] = [media1, media2, media3, media4];
+
+    let arr: string[] = [text];
+
+    if (!verifyArray(arr)) {
+      return res
+        .status(PostErrors.postUserInvalidDetails().details.errorCode)
+        .send(PostErrors.postUserInvalidDetails());
+    }
+
+    const post: Post = {
+      post_uuid: crypto.randomUUID(),
+      author_uuid: uuid,
+      likes: [{}],
+      comments: [{}],
+      text: text,
+      media: media,
+    };
+
+    await prisma.user.update({
+      data: {
+        posts: {
+          push: post.post_uuid,
+        },
+      },
+      where: {
+        uuid: post.author_uuid,
+      },
+    });
+
+    await prisma.post.create({
+      data: {
+        post_uuid: post.post_uuid,
+        author_uuid: post.author_uuid,
+        likes: post.likes,
+        comments: post.comments,
+        text: post.text,
+        media: post.media,
+      },
+    });
+
+    return res.send("success!");
+  }
+);
+
+export default createPost;
