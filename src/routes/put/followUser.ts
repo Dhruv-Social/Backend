343acfca8bece9b9a6cdba1a534c5388515cda26
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-07-02 10:31:00 +1200

    docs: added some comments to the endpoint

--- src/routes/put/followUser.ts
+++ src/routes/put/followUser.ts
@@ -21,6 +21,7 @@ followUser.put("/", authToken, async (req: Request | any, res: Response) => {
       .send(PutErrors.didNotProvideDetails());
   }
 
+  // Get the users following
   const usersFollowing = await prisma.user.findUnique({
     where: {
       uuid: uuidToFollow,
@@ -30,18 +31,21 @@ followUser.put("/", authToken, async (req: Request | any, res: Response) => {
     },
   });
 
+  // If the data is null, then we know the user does not exist
   if (usersFollowing === null) {
     return res
       .status(PutErrors.followUserIncorrectUuid().details.errorCode)
       .send(PutErrors.followUserIncorrectUuid());
   }
 
+  // If the user sending the request to follow is already in that list, then we know we already follow them
   if (usersFollowing?.followers.includes(uuid)) {
     return res
       .status(PutErrors.followUserYouAlreadyFollow().details.errorCode)
       .send(PutErrors.followUserYouAlreadyFollow());
   }
 
+  // Else we just add them to the following list
   await prisma.user.update({
     data: {
       followers: {
