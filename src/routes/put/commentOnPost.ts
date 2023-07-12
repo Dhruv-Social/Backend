c4b17f4a107e93ac08da96a946132f667d770d7d
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-07-12 13:17:29 +1200

    feat: commenting on posts done

--- src/routes/put/commentOnPost.ts
+++ src/routes/put/commentOnPost.ts
@@ -11,42 +11,37 @@ import { IPostComment } from "../../core/data/interfaces";
 const commentOnPost: Router = express.Router();
 
 // Endpoint to create a comment on a post
-commentOnPost.put(
-  "/",
-  authToken,
-  fileUpload(),
-  async (req: Request | any, res: Response) => {
-    const { uuid } = req.user;
-    const { postUuid } = req.query;
-    const { commentText } = req.body;
+commentOnPost.put("/", authToken, async (req: Request | any, res: Response) => {
+  const { uuid } = req.user;
+  const { postUuid } = req.query;
+  const { commentText } = req.body;
 
-    const arr = [commentText, postUuid];
+  const arr = [commentText, postUuid];
 
-    if (!verifyArray(arr))
-      return res
-        .status(PutErrors.didNotProvideDetails().details.errorCode)
-        .send(PutErrors.didNotProvideDetails());
+  if (!verifyArray(arr))
+    return res
+      .status(PutErrors.didNotProvideDetails().details.errorCode)
+      .send(PutErrors.didNotProvideDetails());
 
-    const commentData: IPostComment = {
-      commentUuid: crypto.randomUUID(),
-      authorUuid: uuid,
-      text: commentText,
-      likes: 0,
-    };
+  const commentData: IPostComment = {
+    commentUuid: crypto.randomUUID(),
+    authorUuid: uuid,
+    text: commentText,
+    likes: 0,
+  };
 
-    await prisma.post.update({
-      where: {
-        post_uuid: postUuid,
+  await prisma.post.update({
+    where: {
+      post_uuid: postUuid,
+    },
+    data: {
+      comments: {
+        push: commentData as any,
       },
-      data: {
-        comments: {
-          push: commentData as any,
-        },
-      },
-    });
+    },
+  });
 
-    return res.send("success!");
-  }
-);
+  return res.send("success!");
+});
 
 export default commentOnPost;
