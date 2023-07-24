7f838c272422847af8998308e42139ac5d8aa324
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-07-25 09:00:51 +1200

    feat: document files

--- src/routes/get/reels.ts
+++ src/routes/get/reels.ts
@@ -1,14 +1,19 @@
+// Express
 import express, { Request, Response, Router } from "express";
 import path from "path";
 import fs from "fs";
 
+// Local imports
 import { GetErrors } from "../../core/errors/errors";
 import { authToken } from "../../core/auth/auth";
 
 const reels: Router = express.Router();
 
-// Endpoint to get a users own data
+/* 
+  Endpoint to get a users own data 
+*/
 reels.get("/", authToken, async (req: Request, res: Response) => {
+  // Read the directory
   fs.readdir(
     path.join(`${process.cwd()}/src/public/capybara`),
     (err, files) => {
@@ -17,11 +22,15 @@ reels.get("/", authToken, async (req: Request, res: Response) => {
           .status(GetErrors.reelsError().details.errorCode)
           .send(GetErrors.reelsError());
       }
+
+      // Valyes
       const max = files.length - 1;
       const min = 0;
 
+      // Find get a random number
       const index = Math.round(Math.random() * (max - min) + min);
 
+      // Send the file
       res.sendFile(`${process.cwd()}/src/public/capybara/${files[index]}`);
     }
   );
--- src/routes/get/searchUser.ts
+++ src/routes/get/searchUser.ts
@@ -1,5 +1,7 @@
+// Express
 import express, { Request, Response, Router } from "express";
 
+// Local imports
 import { authToken } from "../../core/auth/auth";
 import { redisClient } from "../../core/redis/redis";
 
@@ -11,27 +13,36 @@ interface redisUser {
   displayName: string;
 }
 
-// Endpoint to get a users data
+/* 
+  Endpoint to search a user in the redis database
+ */
 searchUser.get("/", authToken, async (req: Request, res: Response) => {
+  // Request data
   const { user } = req.query;
   const { uuid } = req.user;
 
   const userData: redisUser[] = [];
 
+  // Get the keys from redis where the key starts with `*user:{key}*`
   const similarUsersArr = await redisClient.keys(`*user:${user}*`);
 
+  // Loop over the users fetched
   for (let i = 0; i < similarUsersArr.length; i++) {
     const userJsonString = await redisClient.get(similarUsersArr[i]);
 
+    // if the json is null, then we know the user can not be found
     if (userJsonString === null) {
       return res.send("User can not be found.");
     }
 
+    // Append this data to the array
     userData.push(JSON.parse(userJsonString));
   }
 
+  // Filter the data so it does not include themself
   const filteredData = userData.filter((user: redisUser) => user.uuid !== uuid);
 
+  // Return the data
   return res.send(filteredData);
 });
 
