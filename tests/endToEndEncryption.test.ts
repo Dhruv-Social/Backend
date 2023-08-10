66a12a6edcab84aa7b456383103b33551e71832a
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-08-11 11:05:26 +1200

    feat: generating RSA public and private key generation for e2ee

--- tests/endToEndEncryption.test.ts
+++ tests/endToEndEncryption.test.ts
@@ -1,9 +1,10 @@
-import { generateKeyPair } from "crypto";
+// Importing crypto and fs module
+import crypto from "crypto";
 
-generateKeyPair(
-  "rsa",
-  {
-    modulusLength: 4096,
+// Generating key files using generateKeyPairSync() method
+function generateKeyFiles() {
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 530,
     publicKeyEncoding: {
       type: "spki",
       format: "pem",
@@ -12,12 +13,53 @@ generateKeyPair(
       type: "pkcs8",
       format: "pem",
       cipher: "aes-256-cbc",
-      passphrase: "top secret",
+      passphrase: "",
     },
-  },
-  (err, publicKey, privateKey) => {
-    console.log(publicKey);
-    console.log("----------------");
-    console.log(privateKey);
-  }
-);
+  });
+  return {
+    publicKey: keyPair.publicKey,
+    privateKey: keyPair.privateKey,
+  };
+}
+
+// Calling Generate keys method
+const { publicKey, privateKey } = generateKeyFiles();
+
+// Encrypting the pased string
+function encryptString(plaintext: string) {
+  // Encrypting data using publicEncrypt() method and a public key
+  const encrypted = crypto.publicEncrypt(publicKey, Buffer.from(plaintext));
+
+  return encrypted.toString("hex");
+}
+
+// Decrypting the passed string with private Key
+function decryptString(ciphertext: string) {
+  // Decrypting data using privateDecrypt() method
+  // and the corresponding private key
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey,
+      passphrase: "",
+    },
+    Buffer.from(ciphertext, "hex")
+  );
+  return decrypted.toString("utf8");
+}
+
+// Following data will be encrypted and decrypted
+const plainText = "Hello there!";
+
+// Calling the below method to encrypt string
+const encrypted = encryptString(plainText);
+
+// Printing the plain text
+console.log("Plaintext:", plainText);
+console.log();
+
+// Printing the encrypted text
+console.log("Encrypted Text: ", encrypted);
+console.log();
+
+// Printing the decrypted text
+console.log("Decrypted Text: ", decryptString(encrypted));
