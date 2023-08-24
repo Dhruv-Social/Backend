fecb1ee0ed9d9964bc7f3d99d74d5b3691a0aa11
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-08-24 14:15:58 +1200

    docs: updated commenting on the refreshtoken login endpoint

--- src/core/auth/refreshLogin.ts
+++ src/core/auth/refreshLogin.ts
@@ -1,5 +1,7 @@
+// Express
 import express, { Request, Response, Router } from "express";
 
+// Login imports
 import { ITokenPayload, ITokenPayloadScopes } from "../data/interfaces";
 import { AuthErrors } from "../errors/authErrors";
 import { verifyArray } from "../verifyArray/verifyArray";
@@ -8,6 +10,7 @@ import { redisClient } from "../redis/redis";
 
 const loginRefresh: Router = express.Router();
 
+// Scopes
 const scopes: ITokenPayloadScopes = {
   dhruv_canUpdateUsers: false,
   dhruv_canDeleteUsers: false,
@@ -21,12 +24,15 @@ const scopes: ITokenPayloadScopes = {
   canDeleteSelf: true,
 };
 
+/* 
+  Endpoint to login a user from their refreshToken
+*/
 loginRefresh.post("/", async (req: Request, res: Response) => {
   const { token } = req.body;
 
-  // Verify that the token is not null
   const arr = [token];
 
+  // Verify that the token is not null
   if (!verifyArray(arr))
     return res
       .status(AuthErrors.didNotProvideDetails().details.errorCode)
